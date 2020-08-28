cdeab180fe19671f6fee7889288fd80a11297351
kampff <adam.kampff@gmail.com>
2020-08-28 11:07:56 +0100

    Seperate front- and backend

--- site/build_site.py
+++ site/build_site.py
@@ -11,6 +11,9 @@ site_path = LBBROOT + '/repo/site/lastblackbox.training'
 # Create site root (if not present)
 ret = subprocess.call(["mkdir", "-p", site_path])
 
+# Set frontend folder
+front_path = LBBROOT + '/repo/site/front'
+
 # Sync images to site
 rsync_call = []
 rsync_call.append("rsync")
@@ -28,21 +31,21 @@ rsync_call.append(site_path)
 ret = subprocess.call(rsync_call)
 
 # Copy index.html
-ret = subprocess.call(["cp", LBBROOT + "/repo/site/html/index.html", site_path + "/"])
+ret = subprocess.call(["cp", front_path + "/html/index.html", site_path + "/"])
 
 # Copy style.css
-ret = subprocess.call(["cp", LBBROOT + "/repo/site/css/style.css", site_path + "/"])
+ret = subprocess.call(["cp", front_path + "/css/style.css", site_path + "/"])
 
 # Sync html pages to site
 
 # Load header
-header_path = LBBROOT + '/repo/site/html/header.html'
+header_path = front_path + '/html/header.html'
 file = open(header_path,"r")
 header_text = file.read()
 file.close()
 
 # Load footer
-footer_path = LBBROOT + '/repo/site/html/footer.html'
+footer_path = front_path + '/html/footer.html'
 file = open(footer_path,"r")
 footer_text = file.read()
 file.close()
@@ -67,6 +70,5 @@ num_written = file.write(header_text)
 num_written = file.write(parsed)
 num_written = file.write(footer_text)
 file.close()
-
 
 #FIN
