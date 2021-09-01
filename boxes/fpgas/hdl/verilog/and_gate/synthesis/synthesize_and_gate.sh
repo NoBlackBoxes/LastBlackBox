dad64e2ffee7b227f3c8463a080f90e34950dfdd
kampff <adam.kampff@gmail.com>
2021-09-01 22:35:07 +0100

    Successful (simple) synthesis

--- boxes/fpgas/hdl/verilog/and_gate/synthesis/synthesize_and_gate.sh
+++ boxes/fpgas/hdl/verilog/and_gate/synthesis/synthesize_and_gate.sh
@@ -5,24 +5,14 @@ set -eu
 NBB_ROOT="/home/kampff/NoBlackBoxes"
 LBB_ROOT=$NBB_ROOT"/repos/LastBlackBox"
 
-# Set CPU and Modules folder
-CPU=$LBB_ROOT"/boxes/fpgas/hdl/verilog/cpu"
-MODULES=$CPU"/modules"
-
 # Create out directory
 mkdir -p bin
-mkdir -p bin/bin
 
 # Copy verilog module(s)
-cp $CPU/cpu.v bin/.
-cp $MODULES/* bin/.
+cp ../and_gate.v bin/.
 
 # Copy constraints file
 cp upduino.pcf bin/.
-
-# Copy memory contents
-cp rom.txt bin/bin/.
-cp ram.txt bin/bin/.
 
 # Verify Verilog
 apio verify --project-dir=bin --board upduino3 --verbose
