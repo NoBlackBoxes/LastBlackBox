0c13f17afe5361ed5b608e961cf1a69db80d5e32
kampff <adam.kampff@gmail.com>
2022-09-27 11:20:47 +0100

    Added mystery music CSV files Hearing box

--- boxes/hearing/i2s/convert.py
+++ boxes/hearing/i2s/convert.py
@@ -1,9 +1,26 @@
 import numpy as np
 from scipy.io import wavfile
 
-samplerate, data = wavfile.read('/home/kampff/test.wav')
-rescale = np.copy(data)
-rescale[:,0] = data[:,0]/4096
-rescale[:,1] = data[:,0]/4096
-rescale_s16 = np.int16(rescale) * 10
-wavfile.write('/home/kampff/out.wav', samplerate, rescale_s16)
+# Specify paths
+in_path = 'sound.wav'
+out_path = 'sound.csv'
+
+# Load WAV
+samplerate, data = wavfile.read(in_path)
+
+## Rescale
+#rescale = np.copy(data)
+#rescale[:,0] = data[:,0]/4096
+#rescale[:,1] = data[:,0]/4096
+#rescale_s16 = np.int16(rescale) * 10
+#wavfile.write('rescale.wav', samplerate, rescale_s16)
+
+# Write CSV
+file = open(out_path, 'w')
+num_samples = data.shape[0]
+for i in range(num_samples-1):
+    n = file.write(str(data[i,0])+',')
+n = file.write(str(data[num_samples-1,0]))
+file.close
+
+#FIN
