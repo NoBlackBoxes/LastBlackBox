a76f340b45cdb6a235ff6411ff675d83ed5f951f
kampff <adam.kampff@gmail.com>
2023-01-24 20:22:42 +0000

    Correct coco typos

--- boxes/learning/transfer/datasets/coco.py
+++ boxes/learning/transfer/datasets/coco.py
@@ -6,7 +6,7 @@ from PIL import Image
 # Specify paths
 coco_folder = '/home/kampff/Dropbox/Voight-Kampff/Technology/Datasets/coco'
 dataset_name = 'val2017'
-annotations_path = coco_folder + '/annotations/person_keypoints_val2017.json'
+annotations_path = coco_folder + '/annotations/person_keypoints_' + dataset_name + '.json'
 images_path = coco_folder + '/val2017'
 
 # Initialize the COCO API
@@ -17,7 +17,7 @@ cat_ids = coco.getCatIds(catNms=['person'])
 img_ids = coco.getImgIds(catIds=cat_ids )
 
 # Keypolint labels
-keypoint_lables = ['nose', 'left eye', 'right eye', 'left ear', 'right ear', 'left shoulder', 'right shoulder', 'left elbow', 'right elbow', 'left wrist', 'right wrist', 'left hip', 'right hip', 'left knee', 'right knee', 'left ankle', 'right ankle']
+keypoint_labels = ['nose', 'left eye', 'right eye', 'left ear', 'right ear', 'left shoulder', 'right shoulder', 'left elbow', 'right elbow', 'left wrist', 'right wrist', 'left hip', 'right hip', 'left knee', 'right knee', 'left ankle', 'right ankle']
 
 # Select annotations of images with only one person with a visible nose
 valid_annotations = []
@@ -52,7 +52,7 @@ for i in range(9):
     img = coco.loadImgs(image_id)[0]
     image = Image.open(images_path + '/' + img['file_name'])
     plt.imshow(image); plt.axis('off')
-    coco.showAnns([valid_annotations[index]])
+    #coco.showAnns([valid_annotations[index]])
     nose_X = valid_annotations[index]['keypoints'][0]
     nose_Y = valid_annotations[index]['keypoints'][1]
     plt.plot(nose_X, nose_Y, 'r+')
