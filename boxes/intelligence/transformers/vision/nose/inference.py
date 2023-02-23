75fa46f310d2aaa952ad251b0e6d17bd0cef4a69
kampff <adam.kampff@gmail.com>
2023-02-23 10:31:30 +0000

    Tweak transform

--- boxes/intelligence/transformers/vision/nose/inference.py
+++ boxes/intelligence/transformers/vision/nose/inference.py
@@ -38,7 +38,7 @@ custom_model.to(device)
 # Specify transforms for inputs
 preprocess = transforms.Compose([
     transforms.ToTensor(),
-    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    transforms.Normalize(mean=[0.5000, 0.5000, 0.5000], std=[0.5000, 0.5000, 0.5000]),
 ])
 
 # Get video capture object
