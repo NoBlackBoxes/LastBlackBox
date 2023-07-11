813fc3162e137e67dc7196a33575d4554e5cb23f
kampff <adam.kampff@gmail.com>
2023-07-11 20:29:55 +0100

    Added some details about "hobby servo" control

--- course/bootcamp/day_2/resources/arduino/servo_test/servo_test.ino
+++ course/bootcamp/day_2/resources/arduino/servo_test/servo_test.ino
@@ -1,6 +1,23 @@
 /*
   Servo Test
+
+  Servos are "easier" to control with a computer, but they hide what is going on.
+
+   - In the beginning, a number of "hobby" motor manufacturers got together ~40 years ago to agree on a "standard" 
+   to make it easier to produce remote control cars and airplanes. Sending digital pulses was easy (even over a radio), 
+   but sending "analog" values was prone to noise and required more expensive transmitters/receivers. They decided that 
+   a 1 ms pulse would be "0", a 1.5 ms pulse would be "90", and a 2 ms pulse would be "180". Therefore, a position servo 
+   (that rotates to a specific angle) just needs to detect the length of pulses it is getting (between 1 and 2 ms) and it 
+   knows exactly what angle to rotate to. Your servos use this same convention, but these pulses set the speed, 1 ms 
+   (full backwards), 1.5 ms (stop), 2 ms (full forwards). The chip inside the servo PCB has circuits that measure the duration 
+   of incoming pulses (on the orange wire) and then generate the commands to a PWM to control motor speed and an H-bridge 
+   to set direction. This chip obeys the "hobby motor" standard. The "servo library" in Arduino is just a convenient way of 
+   generating 1 to 2 ms pulses on specific digital pins. You could generate them however you like (using a for loop with a delay 
+   (in microseconds). In fact, the Arduino "servo library" interprets any value you send that  is greater than 200 (I think) 
+   as a pulse length in microseconds...so sending 1500 should be the same as sending 90. Worth testing...
+   but definitely worth knowing what is going on.
 */
+
 #include <Servo.h>  // This includes the "servo" library
 
 Servo left, right;  // This creates to servo objects, one for each motor
