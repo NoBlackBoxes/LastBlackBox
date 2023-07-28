6e840513f6fee4bfc70c953523f678bab07f7676
kampff <adam.kampff@gmail.com>
2023-07-28 13:00:06 +0100

    Add picamera stream

---
+++ course/cybercerebrum/camera/capture_stream.py
@@ -0,0 +1,30 @@
+import socket
+import time
+
+import libcamera
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+from picamera2.outputs import FileOutput
+
+picam2 = Picamera2()
+video_config = picam2.create_video_configuration({"size": (1280, 720)})
+video_config["transform"] = libcamera.Transform(hflip=0, vflip=1)
+picam2.configure(video_config)
+encoder = H264Encoder(1000000)
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    sock.bind(("0.0.0.0", 10001))
+    sock.listen()
+
+    picam2.encoders = encoder
+
+    conn, addr = sock.accept()
+    stream = conn.makefile("wb")
+    encoder.output = FileOutput(stream)
+    picam2.start_encoder(encoder=encoder)
+    picam2.start()
+    time.sleep(200)
+    picam2.stop()
+    picam2.stop_encoder()
+    conn.close()
