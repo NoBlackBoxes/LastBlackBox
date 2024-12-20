779b13369919e35347c6186be958979cda8516d4
kampff <adam.kampff@gmail.com>
2024-12-20 12:32:23 +0000

    Fix BOM bug

--- course/scripts/materials/generate_BOM_new.py
+++ course/scripts/materials/generate_BOM_new.py
@@ -31,12 +31,12 @@ for course_name in course_names:
     BOM_path = f"{Config.course_root}/_resources/materials/{course.slug}_BOM.csv"
     with open(BOM_path, 'w') as file:
         file.write("Part,Level,Description,Quantity,Datasheet,Supplier,Package,x(mm),y(mm),z(mm)\n")
-        file.write(",,,,,,,,\n")
+        file.write(",,,,,,,,,\n")
         for session in course.sessions:
             for box in session.boxes:
-                file.write(f"{box.name},,,,,,,\n")
+                file.write(f"{box.name},,,,,,,,,\n")
                 for m in box.materials:
                     file.write(f"{m.part},{m.level},{m.description},{m.quantity},{m.datasheet},{m.supplier},{m.package},{m.x},{m.y},{m.z}\n")
-                file.write(",,,,,,,,\n")
+                file.write(",,,,,,,,,\n")
 
 # FIN
