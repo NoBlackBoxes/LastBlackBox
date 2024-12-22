0a373b69d239cbbd7c387a7eba345c24e6485050
kampff <adam.kampff@gmail.com>
2024-12-22 20:03:38 +0000

    Still refactoring session naming...

--- course/versions/enb/sync_with_bootcamp.sh
+++ course/versions/enb/sync_with_bootcamp.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -eu
-rsync -auv --update --delete ~/NoBlackBoxes/LastBlackBox/course/versions/bootcamp/session_1/ ~/NoBlackBoxes/LastBlackBox/course/versions/enb/session_1/
-rsync -auv --update --delete ~/NoBlackBoxes/LastBlackBox/course/versions/bootcamp/session_2/ ~/NoBlackBoxes/LastBlackBox/course/versions/enb/session_2/
-rsync -auv --update --delete ~/NoBlackBoxes/LastBlackBox/course/versions/bootcamp/session_3/ ~/NoBlackBoxes/LastBlackBox/course/versions/enb/session_3/
-rsync -auv --update --delete ~/NoBlackBoxes/LastBlackBox/course/versions/bootcamp/session_4/ ~/NoBlackBoxes/LastBlackBox/course/versions/enb/session_4/
-rsync -auv --update --delete ~/NoBlackBoxes/LastBlackBox/course/versions/bootcamp/session_5/ ~/NoBlackBoxes/LastBlackBox/course/versions/enb/session_5/
-rsync -auv --update --delete ~/NoBlackBoxes/LastBlackBox/course/versions/bootcamp/session_6/ ~/NoBlackBoxes/LastBlackBox/course/versions/enb/session_6/
+rsync -auv --update --delete ~/NoBlackBoxes/LastBlackBox/course/versions/bootcamp/01_electronics/ ~/NoBlackBoxes/LastBlackBox/course/versions/enb/01_session/
+rsync -auv --update --delete ~/NoBlackBoxes/LastBlackBox/course/versions/bootcamp/02_magnetism_semiconductors/ ~/NoBlackBoxes/LastBlackBox/course/versions/enb/02_session/
+rsync -auv --update --delete ~/NoBlackBoxes/LastBlackBox/course/versions/bootcamp/03_session/ ~/NoBlackBoxes/LastBlackBox/course/versions/enb/03_session/
+rsync -auv --update --delete ~/NoBlackBoxes/LastBlackBox/course/versions/bootcamp/04_session/ ~/NoBlackBoxes/LastBlackBox/course/versions/enb/04_session/
+rsync -auv --update --delete ~/NoBlackBoxes/LastBlackBox/course/versions/bootcamp/05_session/ ~/NoBlackBoxes/LastBlackBox/course/versions/enb/05_session/
+rsync -auv --update --delete ~/NoBlackBoxes/LastBlackBox/course/versions/bootcamp/06_session/ ~/NoBlackBoxes/LastBlackBox/course/versions/enb/06_session/
