5a89445f447e85b6b1685a974edc38f9b4f4c783
kampff <adam.kampff@gmail.com>
2025-11-04 13:19:06 +0000

    Added cursor to pyglet plot

--- libs/NB3/Plot/axes.py
+++ libs/NB3/Plot/axes.py
@@ -20,6 +20,7 @@ class Axes:
         self.window = None
         self.x_axis = None
         self.y_axis = None
+        self.cursor = 0.0
 
     def open(self):
         self.window = pyglet.window.Window(self.width, self.height, caption=self.title, vsync=True, resizable=True)
@@ -29,11 +30,13 @@ class Axes:
         gl.glLineWidth(1.0)
         self.batch = pyglet.graphics.Batch()
         self._draw_coords()
+        self._draw_cursor()
 
         @self.window.event
         def on_resize(width, height):
             self.width, self.height = width, height
             self._draw_coords()
+            self._draw_cursor()
             return pyglet.event.EVENT_HANDLED
 
         @self.window.event
@@ -50,6 +53,7 @@ class Axes:
         if self.window:
             self.window.clear()
             self.batch.draw()
+            self._draw_cursor()
             self.window.flip()
 
     def close(self):
@@ -62,5 +66,11 @@ class Axes:
         y0 = self.height/2.0
         self.x_axis = shapes.Line(0, y0, self.width, y0, thickness=2, color=(64, 64, 64), batch=self.batch)
         self.y_axis = shapes.Line(x0, 0, x0, self.height, thickness=2, color=(64, 64, 64), batch=self.batch)
+
+    def _draw_cursor(self):
+        x0 = self.cursor * self.width
+        y0 = 0.0
+        y1 = self.height
+        self.y_axis = shapes.Line(x0, 0, x0, self.height, thickness=4, color=(196, 0, 0), batch=self.batch)
 
 #FIN
