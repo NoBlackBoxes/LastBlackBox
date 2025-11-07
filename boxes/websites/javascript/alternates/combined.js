cb461d324795768f85f688a659c717ff8aad0c5f
kampff <adam.kampff@gmail.com>
2025-11-07 14:09:53 +0000

    Javascript example tweak

--- boxes/websites/javascript/alternates/combined.js
+++ boxes/websites/javascript/alternates/combined.js
@@ -5,9 +5,9 @@ if (toggle_btn) {
         const sidebar = document.querySelector('.sidebar');
         const container = document.querySelector('.container');
 
-        const is_hidden = sidebar.style.display === 'none';
-        sidebar.style.display = is_hidden ? 'block' : 'none';
-        container.classList.toggle('sidebar-hidden', !is_hidden);
+        const is_hidden = sidebar.style.display === 'none';         // Is the sidebar hidden?
+        sidebar.style.display = is_hidden ? 'block' : 'none';       // Toggle visibility
+        container.classList.toggle('sidebar-hidden', !is_hidden);   // Toggle CSS class "sidebar.hidden"
     });
 }
 
--- boxes/websites/javascript/alternates/toggle.js
+++ boxes/websites/javascript/alternates/toggle.js
@@ -5,8 +5,8 @@ if (toggle_btn) {
         const sidebar = document.querySelector('.sidebar');
         const container = document.querySelector('.container');
 
-        const is_hidden = sidebar.style.display === 'none';
-        sidebar.style.display = is_hidden ? 'block' : 'none';
-        container.classList.toggle('sidebar-hidden', !is_hidden);
+        const is_hidden = sidebar.style.display === 'none';         // Is the sidebar hidden?
+        sidebar.style.display = is_hidden ? 'block' : 'none';       // Toggle visibility
+        container.classList.toggle('sidebar-hidden', !is_hidden);   // Toggle CSS class "sidebar.hidden"
     });
 }
