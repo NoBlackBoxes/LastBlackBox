eb3106523efe9eceda891a76254a64e4790b06ad
Adam Kampff <adam.kampff@gmail.com>
2025-11-08 18:29:56 +0000

    Draft work on timing example...

--- boxes/audio/signal-processing/python/02_measure_timing.py
+++ boxes/audio/signal-processing/python/02_measure_timing.py
@@ -1,8 +1,9 @@
 # Measure the timing of volume peaks detected on the left vs. right channel
-# - This timing can contain information about where the sound is coming from
+# - This timing difference can contain information about where the sound is coming from
 import os
 import time
 import numpy as np
+import scipy as sp
 import matplotlib.pyplot as plt
 import NB3.Sound.microphone as Microphone
 import NB3.Sound.utilities as Utilities
@@ -62,22 +63,44 @@ left_peak = np.argmax(left_smooth)      # Get sample with largest value (left ch
 right_peak = np.argmax(right_smooth)    # Get sample with largest value (right channel - smoothed)
 print(left_peak, right_peak)
 
-# Plot volume peak
-padding = 1000
+# Extract window around peak
+padding = 2400
+left_window = recording[(left_peak-padding):(left_peak+padding), 0]
+left_window_smooth = left_smooth[(left_peak-padding):(left_peak+padding)]
+right_window = recording[(right_peak-padding):(right_peak+padding), 1]
+right_window_smooth = right_smooth[(right_peak-padding):(right_peak+padding)]
+
+# Now find the time the sound started by searching for first threshold crossing
+start_threshold = 0.1
+cc = sp.signal.correlate(left_window, right_window, mode='full', method='fft')
+lags = sp.signal.correlation_lags(len(left_window), len(right_window), mode='full')
+delay = lags[np.argmax(cc)]
+print(delay)
+
+# Plot volume peaki(
+y_min = np.min([left_window, right_window]) * 1.1
+y_max = np.max([left_window, right_window]) * 1.1
+
 plt.figure()
 plt.tight_layout()
 
-plt.subplot(2,1,1)
-plt.plot(left_volume[(left_peak-padding):(left_peak+padding)], 'b')
-plt.plot(left_smooth[(left_peak-padding):(left_peak+padding)], 'r')
-plt.ylabel("Volume Peak (Left)")
+plt.subplot(3,1,1)
+plt.plot(left_window, 'b')
+plt.plot(left_window_smooth, 'r')
+plt.ylabel("Peak (Left)")
+plt.ylim(y_min, y_max)
 plt.grid(True)
 
-plt.subplot(2,1,2)
-plt.plot(right_volume[(right_peak-padding):(right_peak+padding)], 'b')
-plt.plot(right_smooth[(right_peak-padding):(right_peak+padding)], 'r')
+plt.subplot(3,1,2)
+plt.plot(right_window, 'b')
+plt.plot(right_window_smooth, 'r')
 plt.xlabel("Sample Number")
-plt.ylabel("Volume Peak (Right)")
+plt.ylabel("Peak (Right)")
+plt.ylim(y_min, y_max)
+plt.grid(True)
+
+plt.subplot(3,1,3)
+plt.plot(cc)
 plt.grid(True)
 
 # Save volume peak timing plot
